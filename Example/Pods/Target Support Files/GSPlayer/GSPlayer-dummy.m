01b4f8f2c3aef3163752c758748194d25cf0aac8
xjbeta <xjbeta.tiny@gmail.com>
2021-07-22 10:42:57 +0800

    Remove useless items.

--- Example/Pods/Target Support Files/GSPlayer/GSPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GSPlayer : NSObject
-@end
-@implementation PodsDummy_GSPlayer
-@end
--- Example/Pods/Target Support Files/Pods-GSPlayer_Example/Pods-GSPlayer_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GSPlayer_Example : NSObject
-@end
-@implementation PodsDummy_Pods_GSPlayer_Example
-@end
